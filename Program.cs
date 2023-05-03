0cc2193841d7a4e4b3eeb0e8635a046368c2d98a
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-05-03 20:07:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex6
+{
+    internal class Program
+
+        {
+            static void Main(string[] args)
+            {
+                double P;
+                double h;
+                double IMC;
+
+                Console.Write("insira o Peso da pessoa: ");
+                P = double.Parse(Console.ReadLine());
+
+                Console.Write("insira o Valor da altura da pessoa: ");
+                h = double.Parse(Console.ReadLine());
+
+                IMC = P / (Math.Pow(h, 2));
+
+                if (IMC < 20)
+                    Console.WriteLine("Abaixo do peso.");
+                else
+                        if (IMC < 25)
+                    Console.WriteLine("Peso ideal");
+                else
+                    Console.WriteLine("Acima do peso");
+                
+
+             
+                
+
+
+
+                
+            }
+        }
+    
+}
+    
+
